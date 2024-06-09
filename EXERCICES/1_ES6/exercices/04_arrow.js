25dac71004752e8cb46ca577510157796e9bf8a3
Naterraa <rddinel@gmail.com>
2024-06-09 21:04:01 +0200

    exos_1

---
+++ EXERCICES/1_ES6/exercices/04_arrow.js
@@ -0,0 +1,23 @@
+
+/**
+ * Déclarer une fonction fléchée qui renvoi l'argument + 1
+ * Le return est explicite
+ * 
+ * contrainte: 
+ *   - le mot clé "function" est interdit
+ */
+
+// const arrow1 ...
+
+/**
+ * Déclarer une fonction fléchée qui renvoi l'argument + 1
+ * Le return est implicite
+ * 
+ * contrainte: 
+ *   - le mot clé "function" est interdit
+ *   - le mot clé "return" est interdit
+ */
+
+// const arrow2 ...
+
+module.exports = {arrow1, arrow2};
